9f1870825131389c3e319463b4d20f3e71bb0e80
Enrico Marino <enrico.marino@email.com>
2013-02-12 16:56:57 +0100

    add RouteManager#bind

--- index.js
+++ index.js
@@ -28,5 +28,31 @@ function RouteManager (router, object) {
 
   this.router = router;
   this.object = object;
-  this.bindings = {};
 }
+
+/**
+ * bind
+ * Bind `route` to  `method`.
+ *
+ * @example
+ *    events.bind('#page/:page', 'onpage')
+ *
+ * @param {String} event `event` name
+ * @param {String} method `method` name
+ * @return {RouteManager} this for chaining
+ * @api public
+ */
+
+RouteManager.prototype.bind = function (route, method) {
+  if (toString.call(route) === '[object Object]') {
+    return this.bind_all(event);
+  }
+  
+  var router = this.router;
+  var object = this.object;
+  var fn = object[method].bind(object);
+
+  router.route(route, fn);
+
+  return this;
+};
